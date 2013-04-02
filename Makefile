71237c1e6d314f063eb22704a336f8cc4dfe7909
vieri <vieri_19@wp.pl>
2013-04-02 20:01:58 +0200

    Start on atolic true studio first commit for clear discovery m0 project.Working.

---
+++ Makefile
@@ -0,0 +1,80 @@
+###############################################################################
+#
+#  	    File        : Makefile
+#
+#       Abstract    : Example Makefile for a C Project
+#
+#       Environment : Atollic TrueSTUDIO(R)
+#
+###############################################################################
+
+SHELL=cmd
+
+# System configuration
+CC = arm-atollic-eabi-gcc
+RM=rm -rf
+
+# Assembler, Compiler and Linker flags and linker script settings
+LINKER_FLAGS=-lm -mthumb -mcpu=cortex-m0  -Wl,--gc-sections -T$(LINK_SCRIPT) -static  -Wl,--start-group -lc -lm -Wl,--end-group -Wl,-cref "-Wl,-Map=$(BIN_DIR)/Zapper_Start.map" -Wl,--defsym=malloc_getpagesize_P=0x1000
+LINK_SCRIPT="stm32_flash.ld"
+ASSEMBLER_FLAGS=-c -g -O0 -mcpu=cortex-m0  -mthumb  -x assembler-with-cpp  -Isrc -ILibraries\CMSIS\Include -ILibraries\CMSIS\Device\ST\STM32F0xx\Include -ILibraries\STM32F0xx_StdPeriph_Driver\inc -IUtilities\STM32F0-Discovery
+COMPILER_FLAGS=-c -g -mcpu=cortex-m0  -O0 -Wall -ffunction-sections -fdata-sections -mthumb -D"USE_STM32F0_DISCOVERY" -D"HSI_VALUE=8000000" -D"STM32F0XX" -D"USE_STDPERIPH_DRIVER"   -Isrc -ILibraries\CMSIS\Include -ILibraries\CMSIS\Device\ST\STM32F0xx\Include -ILibraries\STM32F0xx_StdPeriph_Driver\inc -IUtilities\STM32F0-Discovery 
+
+# Define output directory
+OBJECT_DIR = Debug
+BIN_DIR = $(OBJECT_DIR)
+
+# Define sources and objects
+SRC := $(wildcard */*/*/*/*/*/*/*.c) \
+	$(wildcard */*/*/*/*/*/*.c) \
+	$(wildcard */*/*/*/*/*.c) \
+	$(wildcard */*/*/*/*.c) \
+	$(wildcard */*/*/*.c) \
+	$(wildcard */*/*.c) \
+	$(wildcard */*.c)
+SRCSASM := 	$(wildcard */*/*/*/*/*/*/*/*.s) \
+	$(wildcard */*/*/*/*/*/*/*.s) \
+	$(wildcard */*/*/*/*/*/*.s) \
+	$(wildcard */*/*/*/*/*.s) \
+	$(wildcard */*/*/*/*.s) \
+	$(wildcard */*/*/*.s) \
+	$(wildcard */*/*.s) \
+	$(wildcard */*.s)
+OBJS := $(SRC:%.c=$(OBJECT_DIR)/%.o) $(SRCSASM:%.s=$(OBJECT_DIR)/%.o)
+OBJS := $(OBJS:%.S=$(OBJECT_DIR)/%.o)  
+
+###############
+# Build project
+# Major targets
+###############
+all: buildelf
+
+buildelf: $(OBJS) 
+	$(CC) -o "$(BIN_DIR)/Zapper_Start.elf" $(OBJS) $(LINKER_FLAGS)
+
+clean:
+	$(RM) $(OBJS) "$(BIN_DIR)/Zapper_Start.elf" "$(BIN_DIR)/Zapper_Start.map"
+
+
+##################
+# Specific targets
+##################
+$(OBJECT_DIR)/src/main.o: src/main.c
+	@mkdir $(subst /,\,$(dir $@)) 2> NUL || echo off
+	$(CC) $(COMPILER_FLAGS) src/main.c -o $(OBJECT_DIR)/src/main.o 
+
+
+##################
+# Implicit targets
+##################
+$(OBJECT_DIR)/%.o: %.c
+	@mkdir $(subst /,\,$(dir $@)) 2> NUL || echo off
+	$(CC) $(COMPILER_FLAGS) $< -o $@
+
+$(OBJECT_DIR)/%.o: %.s
+	@mkdir $(subst /,\,$(dir $@)) 2> NUL || echo off
+	$(CC) $(ASSEMBLER_FLAGS) $< -o $@
+	
+$(OBJECT_DIR)/%.o: %.S
+	@mkdir $(subst /,\,$(dir $@)) 2> NUL || echo off
+	$(CC) $(ASSEMBLER_FLAGS) $< -o $@
---
+++ src/main.c
@@ -0,0 +1,152 @@
+/**
+*****************************************************************************
+**
+**  File        : main.c
+**
+**  Abstract    : main function.
+**
+**  Functions   : main
+**
+**  Environment : Atollic TrueSTUDIO(R)
+**
+**  Distribution: The file is distributed “as is,” without any warranty
+**                of any kind.
+**
+**  (c)Copyright Atollic AB.
+**  You may use this file as-is or modify it according to the needs of your
+**  project. This file may only be built (assembled or compiled and linked)
+**  using the Atollic TrueSTUDIO(R) product. The use of this file together
+**  with other tools than Atollic TrueSTUDIO(R) is not permitted.
+**
+*****************************************************************************
+*/
+
+/* Includes */
+#include <stddef.h>
+#include "stm32f0xx_rcc.h"
+#include "stm32f0xx_gpio.h"
+
+/* Private typedef */
+
+/* Private define  */
+/* STM32F0_DISCOVERY board definitions */
+#define LED_PORT GPIOC
+#define LED1 GPIO_Pin_9
+#define LED2 GPIO_Pin_8
+
+#define KEY_PORT GPIOA
+#define KEY GPIO_Pin_0
+
+/* Private macro */
+
+/* Private variables */
+
+/* Private function prototypes */
+
+/* Private functions */
+
+/* Global variables */
+uint32_t timer=0;
+uint8_t  timerFlag=0;
+
+/**
+**===========================================================================
+**
+**  Abstract: SysTick interrupt handler
+**
+**===========================================================================
+*/
+void SysTick_Handler(void)
+{
+  timer++;
+  if  (timer>2000)
+  {
+    timerFlag = 1;
+    timer = 0;
+  }
+}
+
+
+/**
+**===========================================================================
+**
+**  Abstract: main program
+**
+**===========================================================================
+*/
+int main(void)
+{
+  uint32_t ii = 0;
+  GPIO_InitTypeDef        GPIO_InitStructure;
+
+  /* TODO - Add your application code here */
+  SysTick_Config(4800);  /* 0.1 ms = 100us if clock frequency 12 MHz */
+
+  SystemCoreClockUpdate();
+  ii = SystemCoreClock;    /* This is a way to read the System core clock */
+  ii = 0;
+
+  /* GPIOA-C Periph clock enable */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+
+  /* Configure
+   * PA0 USER Button input
+   * */
+  GPIO_InitStructure.GPIO_Pin = KEY;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(KEY_PORT, &GPIO_InitStructure);
+
+  /* Configure PC8 and PC9 in output pushpull mode
+   * PC8 = LD3 Green LED
+   * PC9 = LD4 Blue LED
+   * */
+  GPIO_InitStructure.GPIO_Pin = LED1 | LED2;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(LED_PORT, &GPIO_InitStructure);
+
+  GPIO_SetBits(LED_PORT, LED1);
+  GPIO_ResetBits(LED_PORT, LED1);
+  LED_PORT->BSRR = LED2;
+  LED_PORT->BRR = LED2;
+
+  while (1)
+  {
+	  if (timerFlag)
+	  {
+		  timerFlag = 0;
+		  ii++;
+
+		  /* Toggle LED1 */
+		  if (ii == 1)
+		  {
+			  LED_PORT->BSRR = LED1;
+		  }
+		  else if (ii == 2)
+		  {
+			  ii = 0;
+			  LED_PORT->BRR = LED1;
+		  }
+	  }
+      if(GPIO_ReadInputDataBit(KEY_PORT, KEY))
+	  {
+		  /* USER key pressed */
+		  if (ii == 1)
+		  {
+			  GPIO_SetBits(LED_PORT, LED2);
+		  }
+	  }
+	  else
+	  {
+		  GPIO_ResetBits(LED_PORT, LED2);
+	  }
+  }
+  return 0;
+}
